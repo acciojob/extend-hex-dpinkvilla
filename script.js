a6464f73a22dd63b15c314f56ee08d4bc0b31794
dpinkvilla <117904994+dpinkvilla@users.noreply.github.com>
2024-02-19 20:00:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 const extendHex = (shortHex) => {
   // write your code here
+  let nik= '#' +shortHex
+    .slice(shortHex.startsWith('#') ? 1 : 0)
+    .split('')
+    .map(x => x + x)
+    .join('');
+  
+
+	return nik;
 };
 
 // Do not change the code below.
